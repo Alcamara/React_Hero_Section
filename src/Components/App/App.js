bb9948386e1e8fc489e4a6a76f457bd4ebcd39e3
Al Camara <Alhousseinec@gmail.com>
2023-01-23 15:06:05 -0600

    first commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { HeroSection } from "../HeroSection/HeroSection";
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <header className="App-header">
+        <p>Starter Project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,19 @@
+import "./HeroSection.css";
+
+export function HeroSection() {
+  return (
+    <div className="hero-section">
+      <div className="box">
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+          blanditiis voluptate quam, quae itaque accusantium eum error
+          perferendis doloremque, illo fuga minima est accusamus quis nemo rerum
+          dignissimos laboriosam officia. Vitae ratione repellat illo tempora
+          facere recusandae quae quam obcaecati.{" "}
+        </p>
+      </div>
+    </div>
+  );
+}
